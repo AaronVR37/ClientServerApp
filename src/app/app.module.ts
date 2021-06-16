8bfe3c2b3fcefb85f9ce6318c68e20f0274ad9fc
AaronVR37 <aaron.vargas.is@unipolidgo.edu.mx>
2021-06-16 17:21:20 -0500

    Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,23 @@ import { RegisterComponentComponent } from './auth/register-component/register-c
 import { HeaderComponentComponent } from './shared/header-component/header-component.component';
 import { FooterComponentComponent } from './shared/footer-component/footer-component.component';
 import { NavigationComponentComponent } from './shared/navigation-component/navigation-component.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { RibbonComponent } from './shared/ribbon/ribbon.component';
+
+imports: [
+  BrowserModule,
+  AppRoutingModule,
+  HttpClientModule
+]
 
 @NgModule({
   declarations: [
@@ -15,7 +32,16 @@ import { NavigationComponentComponent } from './shared/navigation-component/navi
     RegisterComponentComponent,
     HeaderComponentComponent,
     FooterComponentComponent,
-    NavigationComponentComponent
+    NavigationComponentComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    NotfoundComponent,
+    ContactComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    RibbonComponent
   ],
   imports: [
     BrowserModule
